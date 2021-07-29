3eb8ea7d2aa38850975609799fcebaa56293a85d
ano201 <ahmu689.com>
2021-07-29 14:44:34 +0600

    test script added

---
+++ main.js
@@ -0,0 +1,11 @@
+const loginBtn = document.getElementById("login");
+const usrName = document.getElementById("user-name");
+const password = document.getElementById("password");
+
+loginBtn.addEventListener("click", function() {
+  if ((usrName.value == "chaos24") && (password.value == "chaotic")) {
+    window.location.href = "https://somudro.netlify.app";
+  } else {
+    console.log("not");
+  }
+})

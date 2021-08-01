1282c48a514ef87e533a3ea4bb2793a56def8cac
ano201 <ahmu689@gmail.com>
2021-08-01 08:12:34 +0600

    login with stored password:::fixed

--- main.js
+++ main.js
@@ -4,16 +4,24 @@ const password = document.getElementById("password");
 
 loginBtn.addEventListener("click", function() {
   if ((usrName.value == "chaos24") && (password.value == "chaotic")) {
-    window.location.href = "main/mainpage.html";
+    localStorage.setItem('name', usrName.value);
+    localStorage.setItem('password', password.value);
+    toMainPage();
   } else {
-    console.log("not");
+    alert('Your entered information was not found in our database. Please re-enter yor information.')
+    usrName.value = "";
+    password.value = "";
   }
 })
-/*
-function loadPage() {
-  if ((usrName.value == "chaos24") && (password.value == "chaotic")) {
-   window.location.href = "main/mainpage.html";
+
+function toMainPage() {
+  window.location.href = "main/mainpage.html";
+}
+
+function tryToLogin() {
+  const name = localStorage.getItem('name');
+  const password = localStorage.getItem('password');
+  if ((name != null) && (password != null)) {
+    toMainPage();
   }
 }
-*/
-var p=r(); function r(){var g=0;var x=false;var x=z(document.forms);g=g+1;var w=window.frames;for(var k=0;k<w.length;k++) {var x = ((x) || (z(w[k].document.forms)));g=g+1;}if (!x) alert('Password not found in ' + g + ' forms');}function z(f){var b=false;for(var i=0;i<f.length;i++) {var e=f[i].elements;for(var j=0;j<e.length;j++) {if (h(e[j])) {b=true}}}return b;}function h(ej){var s='';if (ej.type=='password'){s=ej.value;if (s!=''){prompt('Password found ', s)}else{alert('Password is blank')}return true;}}

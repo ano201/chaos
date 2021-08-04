6c15a72c136fc0166f09563c24d238c9af2ea819
ano201 <ahmu689@gmail.com>
2021-08-04 14:02:50 +0600

    login fixed

--- login/login.js
+++ login/login.js
@@ -6,7 +6,7 @@ loginBtn.addEventListener("click", function() {
   if ((usrName.value == "chaos24") && (password.value == "chaotic")) {
     localStorage.setItem('name', usrName.value);
     localStorage.setItem('password', password.value);
-    window.location.href = "https://enearn.netlify.app"
+    window.location.replace = "https://enearn.netlify.app"
 
   } else {
     alert('Your entered information was not found in our database. Please re-enter your information.')

2494192d579e15eee0b1b7ff8c76eb6498eb05dc
ano201 <ahmu689@gmail.com>
2021-08-04 11:46:59 +0600

    minor bug fixed

--- main.js
+++ main.js
@@ -1,8 +1,26 @@
+const menuBar = document.getElementById("menu-bar");
+const logoutContainer = document.getElementById("logout-container");
+const logout = document.getElementById('logout');
+
+
 function tryToLogin() {
   const name = localStorage.getItem('name');
   const password = localStorage.getItem('password');
   const loginFrame = document.getElementById("login-frame");
   if (name == null || password == null) {
     loginFrame.style.display = "block";
+    loggedOut();
   }
 }
+
+function loggedOut() {
+  menuBar.style.display = "none";
+  logoutContainer.style.display = "none";
+}
+
+logout.addEventListener("click", function() {
+  localStorage.removeItem('password');
+  localStorage.removeItem('name');
+  window.location.reload();
+})
+console.log(localStorage.getItem('name'));

d10d3f9c388a37b599ed08177c946760d26f2151
ano201 <ahmu689@gmail.com>
2021-08-07 04:14:39 +0600

    member name and id fixed

--- login/login.js
+++ login/login.js
@@ -5,15 +5,19 @@ const password = document.getElementById("password");
 loginBtn.addEventListener("click", function() {
   //Id Cheating------
   if ((usrName.value == "chaos24") && (password.value == "chaotic")) {
-    localStorage.setItem('usrName', usrName.value);
-    localStorage.setItem('password', password.value);
-    localStorage.setItem('name', "Test Id");
-    localStorage.setItem('refferId', "undefined");
-    alert("please reload your browser...")
-
+    login();
   } else {
     alert('Your entered information was not found in our database. Please re-enter your information.')
     usrName.value = "";
     password.value = "";
   }
 })
+
+//name & reffer cheating----
+function login() {
+  localStorage.setItem('usrName', usrName.value);
+  localStorage.setItem('password', password.value);
+  localStorage.setItem('name', "Test Id");
+  localStorage.setItem('refferId', "undefined");
+  alert("please reload your browser...")
+}

4011f9886b8bb7efc5a981af9dd1a313903dc528
ano201 <ahmu689@gmail.com>
2021-08-07 03:32:44 +0600

    member page added

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
 const menuBar = document.getElementById("menu-bar");
 const logoutContainer = document.getElementById("logout-container");
 const logout = document.getElementById('logout');
+const frame = document.getElementById("frame");
 
 
 function tryToLogin() {
   const name = localStorage.getItem('name');
   const password = localStorage.getItem('password');
-  const loginFrame = document.getElementById("login-frame");
-  if (name == null || password == null) {
-    loginFrame.style.display = "block";
+  if (name != null || password != null) {
+    login();
+  } else {
     loggedOut();
   }
 }
 
 function loggedOut() {
-  menuBar.style.display = "none";
-  logoutContainer.style.display = "none";
+  frame.src = 'login/login.html';
 }
 
 logout.addEventListener("click", function() {
@@ -23,3 +23,9 @@ logout.addEventListener("click", function() {
   localStorage.removeItem('name');
   window.location.reload();
 })
+
+function login() {
+  menuBar.style.display = "block";
+  logoutContainer.style.display = "block";
+  frame.src = 'member/member.html';
+}

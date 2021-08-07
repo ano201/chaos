2a5e149c0583d6e6514081ab0d271cecf8745d99
ano201 <ahmu689@gmail.com>
2021-08-07 21:08:26 +0600

    3id added

--- login/login.js
+++ login/login.js
@@ -2,22 +2,29 @@ const loginBtn = document.getElementById("login");
 const usrName = document.getElementById("user-name");
 const password = document.getElementById("password");
 
+
+function login(name,reffer) {
+  localStorage.setItem('usrName', usrName.value);
+  localStorage.setItem('password', password.value);
+  localStorage.setItem('name', name);
+  localStorage.setItem('refferId', reffer);
+  alert("please reload your browser...");
+}
+
+
 loginBtn.addEventListener("click", function() {
   //Id Cheating------
   if ((usrName.value == "chaos24") && (password.value == "chaotic")) {
-    localStorage.setItem('name', "Test Id");
-    localStorage.setItem('refferId', "undefined");
-    login();
+    login("Test Id", NaN);
+  } else if ((usrName.value == "somudro") && (password.value == "bilas")) {
+   login("Somudro Bilas","68723");
+  } else if ((usrName.value == "pfahad564") && (password.value == "32423")) {
+    login("Fahad Sheikh", "474369");
+  } else if ((usrName.value == "robbieshapiro") && (password.value == "chaotic")) {
+   login("Robbie","762243");
   } else {
     alert('Your entered information was not found in our database. Please re-enter your information.')
     usrName.value = "";
     password.value = "";
   }
 })
-
-//name & reffer cheating----
-function login() {
-  localStorage.setItem('usrName', usrName.value);
-  localStorage.setItem('password', password.value);
-  alert("please reload your browser...")
-}
